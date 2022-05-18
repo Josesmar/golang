668c4e2b21185a3c5d0d5e69c97aae2dc7b49145
Josesmar <josesmargds@gmail.com>
2022-05-17 21:37:40 -0300

    Exemlos de funções em Go

---
+++ 4 - Funções/funcoes.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func somar(n1, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(2, 5)
+	fmt.Println(soma)
+
+	var f = func(txt string) {
+		fmt.Println(txt)
+	}
+
+	f("texto da função 1")
+	resultadoSoma, _ := calculosMatematicos(10, 10) // usando o anderline é ignorado o valor de retorno da função
+	println(resultadoSoma)
+}
